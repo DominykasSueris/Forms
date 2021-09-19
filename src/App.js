b49c5cc49aefd15e1458ee44ad66931e6c35f1d6
Dominykas Sueris <dominykassueris@Dominykass-MacBook-Air.local>
2021-09-19 21:56:12 +0100

    Form commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import BoxList from './BoxList'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Title!!</h1>
+      <BoxList />
+      </div>
+    )
+  }
+}
+
+export default App;
